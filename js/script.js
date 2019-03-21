d7b9d761fe30aff367f1ec586830d6cdd1f5edc9
CarolinaZ12 <zhagnay@aclanyc.com>
2019-03-21 22:20:43 +0000

    the end?

--- js/script.js
+++ js/script.js
@@ -4,8 +4,17 @@
 /* global $ */
 
 $("#search-button").click(function(){
-  
-  
-  
+    var requested_url="https://api.giphy.com/v1/gifs/search?q=" +$("input").val()+ "&rating=pg&api_key=dc6zaTOxFJmzC";
+    $.ajax({
+  url: requested_url,
+  method:"GET",
+  success:function (response){
+
+      var pic=response.data[0].images.downsized.url;
+    $("body").append("<img src=" +pic+ "/>");
+  }
+    });
+
+
 });
 
